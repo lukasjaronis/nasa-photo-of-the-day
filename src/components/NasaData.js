cc0c316b96726d99d8b02d544ad200473ccca1cd
lukasjaronis <jaronisluke13@gmail.com>
2019-11-06 17:03:23 -0500

    done

--- src/components/NasaData.js
+++ src/components/NasaData.js
@@ -8,12 +8,15 @@ export default function NasaData() {
 
     const [pictures, setPicture] = useState([]);
 
+
+
     useEffect(() => {
 
         Axios
         .get("https://api.nasa.gov/planetary/apod?api_key=81uPCTsZx5S8RPdsRyp15jmrdXXtdfz8fjfC3lfU")
         .then(response => {
-            setPicture(response.data)
+            setPicture(response.data);
+        
         })
         .catch ( error => {
             console.log(`Sorry there seems to be an error! ${error}`);
@@ -22,18 +25,18 @@ export default function NasaData() {
 
     return (
 
-        <div>
+
+        <div className="cardData">
+
 
         <NasaCard 
         
-        key={pictures.index}
+        key={pictures.date}
         title={pictures.title}
         explanation={pictures.explanation}
         image={pictures.hdurl}
         date={pictures.date}
-        
         />
-
         </div>
     );
-}
+    }
